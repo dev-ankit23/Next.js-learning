ba691c778ffd208e168a0b6e54c1a3ee5d5bae34
devankitx <ankitmaurya.coder@gmail.com>
2025-08-21 08:34:02 +0530

    SSR

--- backend-nextjs/app/page.tsx
+++ backend-nextjs/app/page.tsx
@@ -1,8 +1,20 @@
-export default function Home() {
+import axios from "axios";
+
+export default async function Home() {
+  const response = await axios(
+    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+  );
+
+  const data = response.data;
   return (
-    <div>
-      hi there
-      <p>this is a pragraph</p>
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>Name: {data?.name}</div>
+
+          {data?.email}
+        </div>
+      </div>
     </div>
   );
 }
