0e01d30af95637f3d735277a75be305751bf7d22
devankitx <ankitmaurya.coder@gmail.com>
2025-08-21 08:54:26 +0530

    backend in nextjs

--- backend-nextjs/app/page.tsx
+++ backend-nextjs/app/page.tsx
@@ -1,16 +1,14 @@
 import axios from "axios";
 
 export default async function Home() {
-  const response = await axios(
-    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
-  );
+  const response = await axios("http://localhost:3000/api/v1/user/details");
 
   const data = response.data;
   return (
     <div className="flex flex-col justify-center h-screen">
       <div className="flex justify-center">
         <div className="border p-8 rounded">
-          <div>Name: {data?.name}</div>
+          <div>Name: {data?.username}</div>
 
           {data?.email}
         </div>
