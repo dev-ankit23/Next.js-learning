1ad42627e83db9b0be8f6195483c663f13ce56ae
Ankit <ankitmaurya.coder@gmail.com>
2025-08-22 20:47:13 +0530

    Update page.tsx

--- my-app/app/user/page.tsx
+++ my-app/app/user/page.tsx
@@ -3,6 +3,7 @@ export default function User() {
     <>
       <div className=" tex-5xl h-screen text-amber-400 flex justify-items-center justify-content-center ">
         User Profile
+        // busy so doing this to mantain github streak
       </div>
     </>
   );
