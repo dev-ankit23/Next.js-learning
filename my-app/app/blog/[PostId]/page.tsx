77465e82f28273de3c41563f444a015fd4acc05a
devankitx <ankitmaurya.coder@gmail.com>
2025-08-25 08:55:53 +0530

    dynamic routes

---
+++ my-app/app/blog/[PostId]/page.tsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export default async function Blog({ params }: any) {
+  const PostId = (await params).PostId;
+  const response = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts/${PostId}`
+  );
+  const data = response.data;
+
+  return (
+    <div>
+      Blog Page {PostId}
+      <br />
+      title - {data.title}
+      <br />
+      body - {data.body}
+    </div>
+  );
+}
